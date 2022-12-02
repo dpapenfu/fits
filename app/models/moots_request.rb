1771c54f6c82914f0fad9cd505c3a360df9c7da4
Dillon <dpapenfu@chicagobooth.edu>
2022-12-02 23:52:37 +0000

    did a lil more

--- app/models/moots_request.rb
+++ app/models/moots_request.rb
@@ -9,4 +9,9 @@
 #  sender_id    :integer
 #
 class MootsRequest < ApplicationRecord
+  validates(:sender_id, { :presence => true })
+  validates(:recipient_id, { :presence => true })
+  validates(:recipient_id, { :uniqueness => { :scope => ["sender_id"], :message => "already requested" } })
+  belongs_to(:sender, { :required => true, :class_name => "User", :foreign_key => "sender_id", :counter_cache => :sent_follow_requests_count })
+  belongs_to(:recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id", :counter_cache => :received_follow_requests_count })
 end
